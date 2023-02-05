94cf321ae0cdd8a3e687edf451fc55ba59be9561
Lone <94772757+SurvivorJosh@users.noreply.github.com>
2023-02-05 12:32:34 -0500

    Update raider.py

--- raider.py
+++ raider.py
@@ -1,14 +1,100 @@
-import aiohttp, asyncio, os, sys, threading, requests, time, aiohttp, asyncio
+try:
+    import aiohttp, asyncio, os, sys, threading, requests, time, aiohttp, asyncio, random, string, httpx
+    from pystyle                                                             import Add, Center, Anime, Colors, Colorate, Write, System
+    from colorama import Fore, Back, Style
+    import logging
+    
+except:
+    os.system('pip install httpx')
+    os.system('pip install pystyle')
+    os.system('pip install aiohttp')
+    os.system('pip install requests')
+    os.system('pip install colorama')
+    os.system('pip install logging')
+    os.system('py raider.py')
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="\033[38;5;21m[\033[0m%(asctime)s.%(msecs)03d\033[38;5;21m] \033[0m%(message)s\033[0m",
+    datefmt="%H:%M:%S"
+)
+
+captchaApi = "anti-captcha.com"
+captchaKey = ""  # YOUR ANTI CAPTCHA KEY!
+
+System.Clear() 
+os.system('title PRESS ENTER')
+logo = r'''                
+                                           ___  
+                                      (   ) 
+___  ___  ___   .---.   ___ .-.     .-.| |   
+(   )(   )(   ) / .-, \ (   )   \   /   \ |  
+| |  | |  | | (__) ; |  |  .-. .  |  .-. |  
+| |  | |  | |   .'`  |  | |  | |  | |  | |  
+| |  | |  | |  / .'| |  | |  | |  | |  | |  
+| |  | |  | | | /  | |  | |  | |  | |  | |  
+| |  ; '  | | ; |  ; |  | |  | |  | '  | |  
+' `-'   `-' ' ' `-'  |  | |  | |  ' `-'  /  
+ '.__.'.__.'  `.__.'_. (___)(___)  `.__,'   
+                                             
+    
+                                      
+          Join discord.gg/ratio
+    '''
+Anime.Fade(Center.Center(logo), Colors.rainbow, Colorate.Vertical, interval=0.020, enter=True)
+
+
+
+def Captcha():
+    taskId = httpx.post(f"https://api.{captchaApi}/createTask", json={"clientKey": captchaKey, "task": {"type": "HCaptchaTaskProxyless", "websiteURL": "https://discord.com/", "websiteKey": "f5561ba9-8f1e-40ca-9b5b-a0b3f719ef34", "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0"}}, timeout=30).json()
+    if taskId.get("errorId") > 0:
+        print(f"{Fore.RED}{Style.BRIGHT}[-] createTask - {taskId.get('errorDescription')}!{Style.RESET_ALL}")
+        return Captcha()
+            
+    taskId = taskId.get("taskId")
+        
+        
+    captchaData = httpx.post(f"https://api.{captchaApi}/getTaskResult", json={"clientKey": captchaKey, "taskId": taskId}, timeout=30).json()
+    if captchaData.get("status") == "ready":
+        solvedCaptcha = captchaData.get("solution").get("gRecaptchaResponse")
+            
+    return solvedCaptcha
+
+
 
 try: 
     import tasksio
 except:
     os.system('pip install tasksio')
 
-os.system('cls')
+System.Clear()
 
 
-def mainHeader(token):
+    
+    
+def secondHeader(token):
+    return {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9001 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36',
+        'accept': '*/*',
+        'accept-language': 'en-US',
+        'Content-Type': 'application/json',
+        'X-Context-Properties': 'eyJsb2NhdGlvbiI6IkpvaW4gR3VpbGQiLCJsb2NhdGlvbl9ndWlsZF9pZCI6Ijk4OTkxOTY0NTY4MTE4ODk1NCIsImxvY2F0aW9uX2NoYW5uZWxfaWQiOiI5OTAzMTc0ODgxNzg4NjgyMjQiLCJsb2NhdGlvbl9jaGFubmVsX3R5cGUiOjB9',
+        'Authorization': token,
+        'X-Super-Properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRmlyZWZveCIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJmciIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQ7IHJ2OjEwMi4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94LzEwMi4wIiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTAyLjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6IiIsInJlZmVycmluZ19kb21haW4iOiIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTM2MjQwLCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==',
+        'X-Discord-Locale': 'en-US',
+        'X-Debug-Options': 'bugReporterEnabled',
+        'origin': 'https://discord.com',
+        'DNT': '1',
+        'connection': 'keep-alive',
+        'Referer': 'https://discord.com',
+        'cookie': f'__cfduid={os.urandom(43).hex()}; __dcfduid={os.urandom(32).hex()}; locale=en-US',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'TE': 'Trailers',
+    }
+
+def bypassHeader(token):
     return {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
         'Accept': '*/*',
@@ -31,22 +117,39 @@ def mainHeader(token):
         'TE': 'trailers',
     }
     
+
     
-async def token_joiner(token, inv):
+    
+def token_joiner(token, inv):
         
-    headers = mainHeader(token)
-    async with aiohttp.ClientSession() as s:
-        async with s.post(f"https://canary.discord.com/api/v9/invites/{inv}", headers=headers, json={}) as res:
-            if res.status in [200, 201, 204]:
-                print(f"Successfully joined server")
-            elif res.status == 429:
-                f = await res.json()
-                print(f'Ratelimited, retrying in {f["retry_after"]}')
-            elif res.status == 403:
-                print(f'Error, token was locked') 
-            else:
-                print(f"Error couldn't join lol")
-                    
+    headers = bypassHeader(token)
+    
+    res = requests.post(f"https://canary.discord.com/api/v9/invites/{inv}", headers=headers, json={})
+            
+    if res.status_code in [200, 201, 204]:
+        logging.info(f"Succesfully Joined Server | {inv} ")
+    elif res.status_code == 429:
+        try:
+            f = res.json()
+            logging.info(f'Ratelimited, retrying in {f["retry_after"]}')
+        except Exception:
+            logging.info(f'ERROR | MASS RATELIMIT')
+    elif res.status_code == 403:
+        logging.info(f'TOKEN LOCKED | {token}')    
+    else:
+        captcha = Captcha()
+        r = requests.post(f"https://canary.discord.com/api/v9/invites/{inv}", headers=headers, json={"captcha_key": captcha})
+        if r.status_code in [200, 201, 204]:
+            logging.info(f"Succesfully Joined Server | {inv} ")
+        elif r.status_code == 429:
+            
+            f = r.json()
+            logging.info(f'Ratelimited, retrying in {f["retry_after"]}')
+            
+        elif r.status_code == 403:
+            logging.info(f'TOKEN LOCKED | {token}')
+        else:
+            logging.info(f'ERROR | UNKNOWN ERROR')        
 async def mass_join():
     print("Make sure you have valid tokens in tokens.txt")
     inv = input("server invite link -> ")
@@ -56,15 +159,17 @@ async def mass_join():
     except:
         invite = inv
     tokens = open("tokens.txt", "r").readlines()
-    async with tasksio.TaskPool(10_000) as pool:
-        for token in tokens:
-            tok = token.strip()
-            await pool.put(token_joiner(tok, invite))
+   
+    for token in tokens:
+        tok = token.strip()
+        
+        threading.Thread(target=token_joiner, args=(tok, invite,)).start()
     
 def spam(token, channel, message):
     url = 'https://discord.com/api/v9/channels/' + channel + '/messages'
     json = {"content": message, "tts": False}
-    header = mainHeader(token)
+    
+    header = bypassHeader(token)
     while True:
         try:
             r = requests.post(url, headers=header, json=json)
@@ -74,15 +179,13 @@ def spam(token, channel, message):
                 print(f"Ratlimit")
                 time.sleep(float(ratelimit['retry_after']))
             elif r.status_code == 200:
-                print(f'Sent: {message}')
-
+                print(f"Sent | {message}")
             elif r.status_code == 401:
-                print(f'Invalid Token')
+                print(f"Invalid Token")
             elif r.status_code == 404:
-                print('Not Found')
+                print(f"404 |       NOT FOUND")
             elif r.status_code == 403:
-                print(f'Token Doesnt Have Perms For This Channel!')
-                
+                print(f"Token Doesnt Have Perms For This Channel!")
         except:
             pass
         
@@ -146,5 +249,5 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())            
+    asyncio.run(main())         
         
