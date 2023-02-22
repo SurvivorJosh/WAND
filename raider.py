7f383036a11e84eaec829cf0b7ffb74f4d0e7c20
Lone <94772757+SurvivorJosh@users.noreply.github.com>
2023-02-21 21:22:24 -0500

    Update raider.py

--- raider.py
+++ raider.py
@@ -81,7 +81,7 @@ def randstr(lenn):
         text += alpha[random.randint(0, len(alpha) - 1)]
     return text
     
-    
+# HEADERS 
 def thirdHeader(token):
     return {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9001 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36',
